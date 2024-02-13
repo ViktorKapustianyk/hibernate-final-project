a312c128a4a3651a96fadd9fad5b26aad6c6736b
ViktorKapustianyk <vityakap@gmail.com>
2024-02-13 17:47:33 +0200

    created a method that initializes the Redis client and create method pushToRedis / helps each object of the CityCountry type to be written to Radish sequentially

---
+++ src/main/java/org/example/redis/RedisManager.java
@@ -0,0 +1,67 @@
+package org.example.redis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStringCommands;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+public class RedisManager {
+    private SessionFactory sessionFactory;
+    private RedisClient redisClient;
+    private final ObjectMapper mapper;
+
+    public RedisManager() {
+        this.redisClient = prepareRedisClient();
+        this.mapper = new ObjectMapper();
+    }
+
+    public RedisClient prepareRedisClient() {
+        redisClient = RedisClient.create(RedisURI.create("localhost", 6379));
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            System.out.println("\nConnected to Redis\n");
+        }
+        return redisClient;
+    }
+    public void pushToRedis(List<CityCountry> data) {
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            RedisStringCommands<String, String> sync = connection.sync();
+            for (CityCountry cityCountry : data) {
+                try {
+                    sync.set(String.valueOf(cityCountry.getId()), mapper.writeValueAsString(cityCountry));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    public void testRedisData(List<Integer> ids) {
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            RedisStringCommands<String, String> sync = connection.sync();
+            for (Integer id : ids) {
+                String value = sync.get(String.valueOf(id));
+                try {
+                    mapper.readValue(value, CityCountry.class);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void shutdown() {
+        if (nonNull(sessionFactory)) {
+            sessionFactory.close();
+        }
+        if (nonNull(redisClient)) {
+            redisClient.shutdown();
+        }
+    }
+}
