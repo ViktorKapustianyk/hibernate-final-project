f694cafca58548e42d616332362e615f2a2177c5
ViktorKapustianyk <vityakap@gmail.com>
2024-02-13 17:48:42 +0200

    In the CityDAO class, added the getById(Integer id) method, in which we will get the city together with the country:

--- src/main/java/org/example/dao/CityDAO.java
+++ src/main/java/org/example/dao/CityDAO.java
@@ -11,4 +11,10 @@ public class CityDAO extends AbstractHibernateDao<City>{
         Query<Long> query = getCurrentSession().createQuery("select count(c) from City c", Long.class);
         return Math.toIntExact(query.uniqueResult());
     }
+
+    public City getCityById(Integer id) {
+        Query<City> query = getCurrentSession().createQuery("select c from City c join fetch c.country where c.id = :ID", City.class);
+        query.setParameter("ID", id);
+        return query.getSingleResult();
+    }
 }
